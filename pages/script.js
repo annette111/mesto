46e228f16768ee3e9f5132213fbc9988c7414858
annette111 <annette1111111111@yandex.ru>
2023-03-17 00:07:23 +0300

    feat: добавлены новые карточки с помощью js

--- pages/script.js
+++ pages/script.js
@@ -6,7 +6,45 @@ let nameInput = document.querySelector('.popup__input_type_name');
 let infoName = document.querySelector('.profile__info-name');
 let jobInput = document.querySelector('.popup__input_type_about-self');
 let infoSelf = document.querySelector('.profile__info-self');
+let elements = document.querySelector('.elements');
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  initialCards.forEach(function (addCards) {
+    const cardTemplate = document.querySelector('#card-template').content.cloneNode(true);
+    const cardElement = cardTemplate.querySelector('.element');
+    const nameCard = cardElement.querySelector('.element__title');
+    nameCard.textContent = addCards.name;
+    const imageCard = cardElement.querySelector('.element__image');
+    imageCard.setAttribute('src', addCards.link);
+    elements.prepend(cardElement);
+    
+  })
 
 function showPopUp() {
     popUp.classList.add('popup_opened');
@@ -29,3 +67,4 @@ function handleFormSubmit(evt) {
 openPopUp.addEventListener('click', showPopUp);
 formElement.addEventListener('submit', handleFormSubmit);
 closePopUp.addEventListener('click', hidePopUp);
+
